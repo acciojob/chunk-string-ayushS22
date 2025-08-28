0e70b25bc6124435dceee4f88edaac8673b85cf0
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-08-28 17:13:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	 // Convert size to number (prompt gives string)
+  size = Number(size);
+
+  // Edge case: if size <= 0 return whole string
+  if (size <= 0) return [str];
+
+  let chunks = [];
+
+  // Loop through the string in steps of size
+  for (let i = 0; i < str.length; i += size) {
+    chunks.push(str.slice(i, i + size));
+  }
+
+  return chunks;
 }
 
 // Do not change the code below
